674060f04c17a817a2c0c6f323d764f72e823b75
Desi Ramadhani <76545781+desiramadhani715@users.noreply.github.com>
2022-10-23 22:27:22 +0700

    first

--- app/src/main/java/com/crm/terrapro/presentation/auth/AuthActivity.kt
+++ app/src/main/java/com/crm/terrapro/presentation/auth/AuthActivity.kt
@@ -10,7 +10,6 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
-        dc 
 
     }
 }
